005242d5399cf354d8f1f815e942d888f92d3e3f
Nahtan <nathan.titirangi@gmail.com>
2023-11-19 11:44:52 +1300

    better spacing

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ fn map(v: u8, c: u8) -> u8 {
 fn main() {
     let arg = args();
     let args_v: Vec<String> = arg.into_iter().skip(1).collect();
-	if args_v.len() != 4 {
-		panic!("noittf font: str size: f32 clip: u8 out: str\nyou didn't do this and so now we crash\ndo like noittf arial.ttf 32 127 arial");
-	}
+    if args_v.len() != 4 {
+        panic!("noittf font: str size: f32 clip: u8 out: str\nyou didn't do this and so now we crash\ndo like noittf arial.ttf 32 127 arial");
+    }
     let font = &args_v[0];
     let size = args_v[1].parse::<f32>().expect("invalid size");
     let clip = args_v[2].parse::<u8>().expect("invalid clip");
@@ -33,8 +33,7 @@ fn main() {
     let mut height = 0;
     let mut high = 0;
     let mut low = 0;
-	let space_width = font.rasterize(' ',32.0).0.advance_width.round() as i64;
-    for i in 33..=126 {
+    for i in 32..=126 {
         let c = i as u8 as char;
         let (metrics, bitmap) = font.rasterize(c, size);
         let mut new = Vec::new();
@@ -56,14 +55,14 @@ fn main() {
     high -= height as i32;
     println!("{high}");
     low *= -1;
-    width += pictures.len() + space_width as usize + 1;
+    width += pictures.len() + 1;
     let mut buf: Vec<u8> = Vec::new();
     let img = image::codecs::png::PngEncoder::new(&mut buf);
     let bh = height as i32 + high + low;
     let mut image_buf: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> =
         image::ImageBuffer::new(width as u32, bh as u32);
-    let mut xoff;
-    let mut char_id = 33;
+    let mut xoff = 0;
+    let mut char_id = 32;
     let mut xml = format!(
         "
 <FontData>	
@@ -79,22 +78,23 @@ fn main() {
 	<WordSpace>
 		6
 	</WordSpace>
-	<QuadChar id=\"32\" offset_x=\"0\" offset_y=\"0\" rect_h=\"{bh}\" rect_w=\"{space_width}\" rect_x=\"0\" rect_y=\"0\" width=\"{space_width}\" ></QuadChar>
 "
     )
     .to_owned();
-	xoff = space_width + 1;
     for picture in pictures {
-        let picture_img = image::ImageBuffer::from_vec(
-            picture.0.width as u32,
-            picture.0.height as u32,
-            picture.1,
-        )
-        .unwrap();
         let w = picture.0.width;
+        let ow = picture.0.advance_width;
         let ideal_height = high - picture.0.ymin - picture.0.height as i32 + height as i32;
-        image::imageops::overlay(&mut image_buf, &picture_img, xoff, ideal_height as i64);
-        xml.push_str(&format!("\t<QuadChar id=\"{char_id}\" offset_x=\"0\" offset_y=\"0\" rect_h=\"{bh}\" rect_w=\"{w}\" rect_x=\"{xoff}\" rect_y=\"0\" width=\"{w}\" ></QuadChar>\n"));
+        if picture.0.width != 0 && picture.0.height != 0 {
+            let picture_img = image::ImageBuffer::from_vec(
+                picture.0.width as u32,
+                picture.0.height as u32,
+                picture.1,
+            )
+            .unwrap();
+            image::imageops::overlay(&mut image_buf, &picture_img, xoff, ideal_height as i64);
+        }
+        xml.push_str(&format!("\t<QuadChar id=\"{char_id}\" offset_x=\"0\" offset_y=\"0\" rect_h=\"{bh}\" rect_w=\"{w}\" rect_x=\"{xoff}\" rect_y=\"0\" width=\"{ow}\" ></QuadChar>\n"));
         xoff += picture.0.width as i64 + 1;
         char_id += 1;
     }
